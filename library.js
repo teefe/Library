06452d04f9d0a072aaa0c8a66e969fbbdb59a4e0
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-07-29 07:35:45 +0300

    UI Building

---
+++ library.js
@@ -0,0 +1,12 @@
+let myLibrary = []
+
+function Book(tittle,author,pages,read){
+    this.tittle = tittle
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(){
+    
+}
