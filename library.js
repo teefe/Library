97154d684af54aaf1a079099aea2b7d45e930ddc
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-07-30 19:49:38 +0300

    Getting data from the form

--- library.js
+++ library.js
@@ -1,12 +1,38 @@
-let myLibrary = []
+const tittle = document.getElementById('title');
+const author = document.getElementById('author');
+const pages = document.getElementById('pages');
+const read = document.getElementById('Read');
+const BTN = document.getElementById('BTN');
+let newTitle;
+let newAuthor;
+let newPages;
+let newRead;
+let myLibrary = [];
 
-function Book(tittle,author,pages,read){
-    this.tittle = tittle
+function Book(title,author,pages,read){
+    this.title = title
     this.author = author
     this.pages = pages
     this.read = read
 }
 
-function addBookToLibrary(){
-    
-}
+tittle.addEventListener('change',()=>{
+    newTitle = tittle.value;
+    myLibrary.push(newTitle);
+    console.log(newTitle)
+})
+
+author.addEventListener('change',()=>{
+    newAuthor = author.value;
+    console.log(newAuthor)
+})
+
+pages.addEventListener('change',()=>{
+    newPages = pages.value;
+    console.log(newPages)
+})
+read.addEventListener('change',()=>{
+    newRead = read.checked;
+    console.log(newRead)
+})
+
