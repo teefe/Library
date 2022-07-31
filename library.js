923124decd6654eb99fce3bb46ff05de3038f886
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2022-07-31 14:28:08 +0300

    Form data well collected in the object. And object into the array

--- library.js
+++ library.js
@@ -1,13 +1,15 @@
+let myLibrary = [];
+let newTitle;
+let newAuthor;
+let newPages;
+let newRead;
+
 const tittle = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('Read');
 const BTN = document.getElementById('BTN');
-let newTitle;
-let newAuthor;
-let newPages;
-let newRead;
-let myLibrary = [];
+const myForm = document.querySelector('.form') 
 
 function Book(title,author,pages,read){
     this.title = title
@@ -16,23 +18,44 @@ function Book(title,author,pages,read){
     this.read = read
 }
 
+const theBook = new Book(newTitle,newAuthor,newPages,newRead)
+
+function addBookToLibrary() {
+    myLibrary.push(theBook)
+    console.log(theBook)
+}
+
+BTN.addEventListener('onclick',()=>{
+    addBookToLibrary()
+})
+
 tittle.addEventListener('change',()=>{
     newTitle = tittle.value;
-    myLibrary.push(newTitle);
-    console.log(newTitle)
+    theBook.title = newTitle;
 })
 
 author.addEventListener('change',()=>{
     newAuthor = author.value;
-    console.log(newAuthor)
+    theBook.author = newAuthor;
 })
 
 pages.addEventListener('change',()=>{
     newPages = pages.value;
-    console.log(newPages)
+    theBook.pages = newPages;
+    console.log(theBook)
 })
 read.addEventListener('change',()=>{
     newRead = read.checked;
-    console.log(newRead)
+    theBook.read = newRead;
+    console.log(theBook)
 })
 
+const addBtn = document.querySelector('.add-btn')
+
+addBtn.addEventListener('click',()=>{
+    myForm.style.opacity = 1;
+    alert('uyhgenbdshxghYTGHEDSICUJHS')
+})
+
+function formShow() {
+}
